4b63e568d36e7eb20098b77631a59dfbc4fc0073
EPIC\yasiru_l <yasiru_d@epiclanka.net>
2023-05-22 12:36:33 +0530

    Initial commit

---
+++ src/main/java/com/epic/test/controller/LoggingController.java
@@ -0,0 +1,31 @@
+/**
+ * Author : yasiru_l
+ * Date : 5/22/2023
+ * Time : 10:06 AM
+ * Project Name : test
+ */
+
+package com.epic.test.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoggingController {
+
+    Logger logger = LoggerFactory.getLogger(LoggingController.class);
+
+    @RequestMapping("/")
+    public String index() {
+        logger.trace("A TRACE Message");
+        logger.debug("A DEBUG Message");
+        logger.info("An INFO Message");
+        logger.warn("A WARN Message");
+        logger.error("An ERROR Message");
+
+        return "Howdy! Check out the Logs to see the output...";
+    }
+}
+
